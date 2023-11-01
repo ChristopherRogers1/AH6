812642aa6e4a05207183999587ea5ba49ec4536b
ChristopherRogers1 <60368736+ChristopherRogers1@users.noreply.github.com>
2023-11-01 15:41:34 -0400

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,35 +1,44 @@
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.transform.Source;
-
 class Node<T> {
-  T val;
-  Node<T> next;
+    T val;
+    Node<T> next;
 
-  public Node(T val) {
-    this.val = val;
-    this.next = null;
-  }
+    public Node(T val) {
+        this.val = val;
+        this.next = null;
+    }
 }
 
 class Solution {
-  public static List<String> linkedListValues(Node<String> head) {
-    // todo
-    return;
-  }
+    public static List<String> linkedListValues(Node<String> head) {
+        Node<String> test = head;
+        List myList = new ArrayList();
+        myList.add(test);
+        while (test.next!=null) {
+            myList.add(test.next);
+            test = test.next;
+        }
 
-  public static void main() {
-    Node<String> a = new Node<>("a");
-    Node<String> b = new Node<>("b");
-    Node<String> c = new Node<>("c");
-    Node<String> d = new Node<>("d");
-    a.next = b;
-    b.next = c;
-    c.next = d;
+        return myList;
+    }
 
-    // a -> b -> c -> d
+    public static void main(String[] args) {
+        Node<String> a = new Node<>("a");
+        Node<String> b = new Node<>("b");
+        Node<String> c = new Node<>("c");
+        Node<String> d = new Node<>("d");
+        a.next = b;
+        b.next = c;
+        c.next = d;
 
-    Solution.linkedListValues(a); 
-    // -> [ "a", "b", "c", "d" ]
-  }
+        // a -> b -> c -> d
+
+        List myList = Solution.linkedListValues(a);
+        // -> [ "a", "b", "c", "d" ]
+
+        System.out.println(myList);
+
+    }
 }
